19adce460492f318e3252a4a772b48a0f35b3b74
Saritha <sarithayelgam88@gmail.com>
2020-01-20 17:03:38 -0800

    Updates

---
+++ precache-manifest.dfbb2ecf38d76a4fd644c2701d17302b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d715621296b5d7ecf343fbd4683d5161",
+    "url": "/flag-picker/index.html"
+  },
+  {
+    "revision": "d41c04f2c462d653fbb9",
+    "url": "/flag-picker/static/css/main.c0f1ef00.chunk.css"
+  },
+  {
+    "revision": "4c72dc109029a2301908",
+    "url": "/flag-picker/static/js/2.4a5833c5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/flag-picker/static/js/2.4a5833c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d41c04f2c462d653fbb9",
+    "url": "/flag-picker/static/js/main.b4916606.chunk.js"
+  },
+  {
+    "revision": "466c007f9b2154475508",
+    "url": "/flag-picker/static/js/runtime-main.534140b2.js"
+  }
+]);
